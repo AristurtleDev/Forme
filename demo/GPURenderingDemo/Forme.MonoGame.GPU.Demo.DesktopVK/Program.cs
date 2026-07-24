16b941df67ead191aabc34b985d1ad62770fd2e1
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2026-07-24 15:11:58 -0400

    Add demo stubs

---
+++ demo/GPURenderingDemo/Forme.MonoGame.GPU.Demo.DesktopVK/Program.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Christopher Whitley (AristurtleDev). All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using Forme.MonoGame.GPU.Demo;
+
+using var game = new DemoGame();
+game.Run();
